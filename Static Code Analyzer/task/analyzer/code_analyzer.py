12a74785462e5458ef93426a3bf9bbd1e01e371a
Ansis Maleckis <maleckisansis@gmail.com>
2022-09-14 12:02:42 +0300

    formatted according to PyLint

--- Static Code Analyzer/task/analyzer/code_analyzer.py
+++ Static Code Analyzer/task/analyzer/code_analyzer.py
@@ -1,10 +1,14 @@
-def main():
-    filename = input()
-    file = open(filename, "r")
-    for index, line in enumerate(file):
-        if len(line) > 79:
-            print("Line {}: S001 Too long".format(index + 1))
+"""
+Module's docstring.
+"""
 
+def main():
+    '''Main function of the programm'''
+    filename = input()
+    with open(filename, "r", encoding="utf-8") as file:
+        for index, line in enumerate(file):
+            if len(line) > 79:
+                print(f"Line {index + 1}: S001 Too long")
 
 if __name__ == "__main__":
     main()
